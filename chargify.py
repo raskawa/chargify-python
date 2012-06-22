4c6a2f87c4193df8072a949dde1ddaed4860a2ac
Numan S <numan@7geese.com>
2012-06-21 17:07:41 -0700

    URL encode lists properly

--- chargify.py
+++ chargify.py
@@ -61,7 +61,7 @@ class ChargifyHttpClient(object):
     """
     Extracted from the main Chargify class so it can be stubbed out during testing.
     """
-    
+
     def make_request(self, url, method, data, api_key):
         """
         Actually responsible for making the HTTP request.
@@ -71,7 +71,7 @@ class ChargifyHttpClient(object):
         """
         opener = urllib2.build_opener(urllib2.HTTPHandler)
         request = urllib2.Request(url=url, data=data)
-        
+
         # Build header
         request.get_method = lambda: method
         request.add_header('Authorization', 'Basic %s' % base64.encodestring('%s:%s' % (api_key, 'x'))[:-1])
@@ -80,7 +80,7 @@ class ChargifyHttpClient(object):
         request.add_header('Content-Type', 'application/json')
         if data is None:
             request.add_header('Content-Length','0')
-        
+
         # Make request and trap for HTTP errors
         try:
             response = opener.open(request)
@@ -99,9 +99,9 @@ class ChargifyHttpClient(object):
         if response.code in ERROR_CODES and ERROR_CODES[response.code] is not False:
             error_class = ERROR_CODES[e.code]
             raise error_class(data)
-            
+
         return data
-        
+
 class Chargify(object):
     """
     A client for the Chargify API.
@@ -111,19 +111,19 @@ class Chargify(object):
     path = []
     domain = 'https://%s.chargify.com/'
     client = None
-    
+
     def __init__(self, api_key, sub_domain, path=None, client=None):
         """
         :param api_key: The API key for your Chargify account.
         :param sub_domain: The sub domain of your Chargify account.
-        :param path: The current path constructed for this request. 
+        :param path: The current path constructed for this request.
         :param client: The HTTP client to use to make the request.
         """
         self.api_key = api_key
         self.sub_domain = sub_domain
         self.path = path or []
         self.client = client or ChargifyHttpClient()
-    
+
     def __getattr__(self, attr):
         """
         Uses attribute chaining to help construct the url path of the request.
@@ -138,41 +138,41 @@ class Chargify(object):
         :param kwargs: The arguments passed into the request. Valid values are:
             'customer_id', 'product_id', 'subscription_id', 'component_id', 'handle' will be extracted
             and placed into the url. 'data' will be serialized into a JSON string and POSTed with
-            the request. 
+            the request.
         """
         path = self.path[:]
-        
+
         # Find the HTTP method if we were called with create(), update(), read(), or delete()
         if path[-1] in VERBS.keys():
             action = path.pop()
             method = VERBS[action]
         else:
             method = 'GET'
-            
+
         # Extract certain kwargs and place them in the url instead
         for identifier, name in IDENTIFIERS.items():
             value = kwargs.pop(identifier, None)
             if value:
                 path.insert(path.index(name)+1, str(value))
-                
+
         # Convert the data to a JSON string
         data = kwargs.pop('data', None)
         if data:
             data = json.dumps(data)
-            
+
         # Build query string
         if method == 'GET' and kwargs:
-            args = "?%s" % (urllib.urlencode(kwargs.items()))
+            args = "?%s" % (urllib.urlencode(kwargs, True))
         else:
             args = ''
-            
+
         # Build url
         url = self.domain % self.sub_domain
         url = url + '/'.join(path) + '.json' + args
-        
+
         return url, method, data
-            
+
     def __call__(self, **kwargs):
         url, method, data = self.construct_request(**kwargs)
         return self.client.make_request(url, method, data, self.api_key)
-        
+
